062a60509506f82bc9d413e5e8cd453c157c1576
jeewan99 <er.jeewantaj@gmail.com>
2026-05-22 21:43:43 +0545

    remove path in vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio_website_source_code/",
+  base: "/",
   server:{
     host:true, 
     port:5173,
